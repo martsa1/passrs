8b1cb7439c6b353f7bc8d77c23d7d476bbf7d427
Sam Martin-Brown <Nivekkas@gmail.com>
2023-05-23 14:43:45 +0100

    Add super-basic iced GUI demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, text, Column};
+use iced::{Element, Sandbox, Settings};
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Counter {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        "Sample Counter".into()
+    }
+
+    fn view(&self) -> Element<Message> {
+        column![
+            button("+").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("-").on_press(Message::DecrementPressed),
+        ]
+        .into()
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+fn main() -> iced::Result {
+    Counter::run(Settings::default())
 }
