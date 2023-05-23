2aa2a3cfbae539096215529f855249fc5100bbc6
Sam Martin-Brown <Nivekkas@gmail.com>
2023-05-23 14:45:26 +0100

    tweak shell.nix - though not currently using it

--- shell.nix
+++ shell.nix
@@ -5,14 +5,13 @@ pkgs.mkShell rec {
     clang
     llvmPackages.bintools
     rustup
-    rust-analyzer
   ];
   RUSTC_VERSION = pkgs.lib.readFile ./.rust-toolchain;
   # https://github.com/rust-lang/rust-bindgen#environment-variables
   LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
   shellHook = ''
     export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION/bin/
   '';
 
   # If needing pre-compiled library support:
